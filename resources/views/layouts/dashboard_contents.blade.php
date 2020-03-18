2f148927375a89cabbb4792bb42513af3bb98fee
winterdev001 <snowdevin.sd@gmail.com>
2020-03-18 11:13:53 +0200

    added changes on dashboard

--- resources/views/layouts/dashboard_contents.blade.php
+++ resources/views/layouts/dashboard_contents.blade.php
@@ -159,7 +159,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="/dashboard" class="brand-link">
       <img src="{{ asset('images/icons/logo-0.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">Hamubere</span>
